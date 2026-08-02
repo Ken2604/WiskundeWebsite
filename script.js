2d94d3be336ed51d343f8d928c9ae40e9d1ca135
Global Name <kenrob000@gmail.com>
2026-08-02 14:56:53 +0200

    Inhoud index.html en script.js hersteld

---
+++ script.js
@@ -0,0 +1,9 @@
+// Wiskunde Website - Algemene Logica
+console.log("WiskundeWebsite is succesvol geladen!");
+
+// Hier kunnen we later interactieve rekenmodules, dynamische knoppen 
+// of formules voor Wiskunde A en B aan toevoegen.
+document.addEventListener("DOMContentLoaded", function() {
+    // Welkomstmelding in de browser-console (voor controle)
+    console.log("Pagina-elementen zijn gereed.");
+});
